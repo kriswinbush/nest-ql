1d01b562a83b0581f83deaf47efcf26a1706782b
Kris Winbush <khz@kriss-mbp.lan>
2021-08-18 12:13:20 -0700

    initial commit for nest-ql project

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { CatsModule } from './cats/cats.module';
+import { MongooseModule } from '@nestjs/mongoose';
+@Module({
+  imports: [
+    CatsModule,
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    GraphQLModule.forRoot({ autoSchemaFile: 'schema.gql' }),
+  ],
+  /* controllers: [AppController],
+  providers: [AppService], */
+})
+export class AppModule {}
